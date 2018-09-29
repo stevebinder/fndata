a58b782b51945ba8481bc29eff78965bf82bb5b9
Steve Binder <email@stevebinder.com>
2018-09-29 15:41:37 -0400

    Fix ellipsis

--- src/ellipsis/tests.js
+++ src/ellipsis/tests.js
@@ -2,26 +2,20 @@ import ellipsis from './';
 import isString from 'src/isString';
 
 export default {
-  'always return a string': [
+  'should return a string': [
     true,
     () => isString(ellipsis()),
-    () => isString(ellipsis(null)),
-    () => isString(ellipsis(true)),
-    () => isString(ellipsis(1)),
-    () => isString(ellipsis({})),
-    () => isString(ellipsis([])),
-    () => isString(ellipsis(() => {})),
   ],
-  'do not truncate if under or equal to the limit': [
+  'should not truncate if under or equal to the limit': [
     'hello world',
     () => ellipsis('hello world'),
     () => ellipsis('hello world', 11),
   ],
-  'truncate if over the limit': [
+  'should truncate if over the limit': [
     'hello worl...',
     () => ellipsis('hello world', 10),
   ],
-  'use special characters': [
+  'should use special characters for ellipsis': [
     'hello worl_',
     () => ellipsis('hello world', 10, '_'),
   ],
