87d501d4ee6a87a9feae5ed503dfb1cab9080d52
Steve Binder <email@stevebinder.com>
2018-10-14 14:51:54 -0400

    Add prefill

---
+++ src/prefill/tests.js
@@ -0,0 +1,22 @@
+import prefill from './';
+import isString from 'src/isString';
+
+export default {
+  'always convert the value to a string': [
+    true,
+    () => isString(prefill()),
+    () => isString(prefill(2)),
+    () => isString(prefill('a')),
+  ],
+  'prefill to reach length': [
+    ' hello',
+    () => prefill('hello', 6),
+    () => prefill(' hello', 6),
+    () => prefill(' hello', 5),
+  ],
+  'prefill using a special character': [
+    '00123',
+    () => prefill(123, 5, 0),
+    () => prefill(123, 5, '0'),
+  ],
+};
