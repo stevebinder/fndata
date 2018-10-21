2653f86ec660986496bd77a590652fc7775eb9b0
Steve Binder <email@stevebinder.com>
2018-10-20 23:41:36 -0400

    Change capitalize

--- src/capitalize/index.js
+++ src/capitalize/index.js
@@ -1,13 +1,23 @@
 import compose from 'src/compose';
+import curry from 'src/curry';
+import map from 'src/map';
+import join from 'src/join';
 import slice from 'src/slice';
+import split from 'src/split';
 import toString from 'src/toString';
 import upper from 'src/upper';
 
 export default compose(
   toString,
-  value => {
-    const first = slice(value, 0, 1).toUpperCase();
+  [split, ' '],
+  [map, value => {
+    const first = curry(
+      value,
+      [slice, 0, 1],
+      upper,
+    );
     const rest = slice(value, 1);
     return `${first}${rest}`;
-  },
+  }],
+  [join, ' '],
 );
--- src/capitalize/tests.js
+++ src/capitalize/tests.js
@@ -2,7 +2,7 @@ import capitalize from './';
 
 export default {
   'capitalize the first letter only': [
-    'HellO',
-    () => capitalize('hellO'),
+    'HellO WOrld',
+    () => capitalize('hellO wOrld'),
   ],
 };
